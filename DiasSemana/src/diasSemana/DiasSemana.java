bb2592bd6e6454fa0874f75dff47de5b20460c17
danielcalatravagonzalez <degixcala@gmail.com>
2022-02-22 19:40:59 +0100

    Ejercicio 3: He creado el método decisionDia

--- DiasSemana/src/diasSemana/DiasSemana.java
+++ DiasSemana/src/diasSemana/DiasSemana.java
@@ -14,8 +14,8 @@ import java.util.Scanner;
 public class DiasSemana {
 	public static void main(String[] args) {
 		try (Scanner scanner = new Scanner(System.in)) {
-			int n;
-			String s = null;
+			int diaIntroducido;
+			String diaSemana = null;
 // Solicitud de datos al usuario
 			System.out.println("El programa te devuelve el día de la semana seleccionando:");
 			System.out.println("1.- Lunes.");
@@ -27,34 +27,42 @@ public class DiasSemana {
 			System.out.println("7.- Domingo.");
 			System.out.println("_____________________________________________________________");
 			System.out.print("Dime un número y te digo a qué día de la semana corresponde: ");
-			n = scanner.nextInt();
+			diaIntroducido = scanner.nextInt();
 // Realizamos comparaciones y mostramos el resultado
-			switch (n) {
-			case 1:
-				s = "El día seleccionado es LUNES";
-				break;
-			case 2:
-				s = "El día seleccionado es MARTES";
-				break;
-			case 3:
-				s = "El día seleccionado es MIÉRCOLES";
-				break;
-			case 4:
-				s = "El día seleccionado es JUEVES";
-				break;
-			case 5:
-				s = "El día seleccionado es VIERNES";
-				break;
-			case 6:
-				s = "El día seleccionado es SÁBADO";
-				break;
-			case 7:
-				s = "El día seleccionado es DOMINGO";
-				break;
-			default:
-				s = "ERROR: número incorrecto.";
-			}
-			System.out.println(s);
+			decisionDia(diaIntroducido);
 		}
+	}
+
+	/**
+	 * @param numerodia
+	 */
+	public static void decisionDia(int numerodia) {
+		String diaSemana;
+		switch (numerodia) {
+		case 1:
+			diaSemana = "El día seleccionado es LUNES";
+			break;
+		case 2:
+			diaSemana = "El día seleccionado es MARTES";
+			break;
+		case 3:
+			diaSemana = "El día seleccionado es MIÉRCOLES";
+			break;
+		case 4:
+			diaSemana = "El día seleccionado es JUEVES";
+			break;
+		case 5:
+			diaSemana = "El día seleccionado es VIERNES";
+			break;
+		case 6:
+			diaSemana = "El día seleccionado es SÁBADO";
+			break;
+		case 7:
+			diaSemana = "El día seleccionado es DOMINGO";
+			break;
+		default:
+			diaSemana = "ERROR: número incorrecto.";
+		}
+		System.out.println(diaSemana);
 	}
 }
